8a3535c4e80f3e7996fd44f49f70da98bd2f729a
Wastend <wastend318@gmail.com>
2023-07-31 18:25:13 +0600

    header ready

--- src/app/components/layout/Header/Header.jsx
+++ src/app/components/layout/Header/Header.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Logo from '../../../assets/Logo.png'
+import React, { useState } from 'react'
+import logo from '../../../assets/logo.png'
+import menu_buttom from '../../../assets/menu_buttom.png'
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const click__menu = () => {
+    setOpen(!open)
+  }
+
   return (
     <header>
-      <a href='/' ><img className='header__logo' src={Logo} alt="Logo" /></a>
-      <div className="header__links">
-        <a href="/articles">Новости</a>
-        <a href='/profile'>Профиль</a>
-      </div>
+      <a href='/' ><img className='logo' src={logo} alt="Logo" /></a>
+      <nav className={`nav ${open && 'open'}`}>
+        <ul className="nav__list">
+          <li ><a className='nav__link' href="/articles">Новости</a></li>
+          <li ><a className='nav__link' href='/profile'>Профиль</a></li>
+        </ul>
+        <button onClick={click__menu} className="nav__button">
+          <img src={menu_buttom} alt="menu_buttom" />
+        </button>
+      </nav>
+
     </header>
   )
 }
--- src/pages/MainPage/MainStart.jsx
+++ src/pages/MainPage/MainStart.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import watch__silver from '../../app/assets/watch__silver.png'
 
 const MainStart = () => {
   return (
-    <div className='StartPage'>
-      
+    <div className='start'>
+      <div className="start__info">
+        <p className="start__text">Часовое ателье Дмитрия Котова</p>
+      </div>
+      <img src={watch__silver} className="start__image" alt='watch__silver'></img>
     </div>
   )
 }
