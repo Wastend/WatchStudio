90e54b62548da5305c1a42eff0c18d5f220ed623
Wastend <wastend318@gmail.com>
2023-07-31 22:44:36 +0600

    do start page, revorke, bad push

--- src/app/components/layout/Header/Header.jsx
+++ src/app/components/layout/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import logo from '../../../assets/logo.png'
-import menu_buttom from '../../../assets/menu_buttom.png'
+import menu_button from '../../../assets/menu_button.png'
 
 const Header = () => {
   const [open, setOpen] = useState(false)
@@ -11,14 +11,14 @@ const Header = () => {
 
   return (
     <header>
-      <a href='/' ><img className='logo' src={logo} alt="Logo" /></a>
+      <a className='logo' href='/' ><img src={logo} alt="Logo" /></a>
       <nav className={`nav ${open && 'open'}`}>
         <ul className="nav__list">
           <li ><a className='nav__link' href="/articles">Новости</a></li>
           <li ><a className='nav__link' href='/profile'>Профиль</a></li>
         </ul>
         <button onClick={click__menu} className="nav__button">
-          <img src={menu_buttom} alt="menu_buttom" />
+          <img src={menu_button} alt="menu_button" />
         </button>
       </nav>
 
